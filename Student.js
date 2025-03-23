bdb0b4d9cfab8c1709568896c3b20fab14579c5e
Anushka Kumari <anushkakumari1962003@gmail.com>
2025-03-23 12:10:47 +0530

    student Manager System

---
+++ Student.js
@@ -0,0 +1,44 @@
+function createStudentManager(){
+    let student = {}
+
+    return{
+        addStudent(name){
+            if(students[name]){
+                students[name] = {subjsct :{}}
+            }
+        }
+        addSubject(name, subject, score){
+            if(students[name]){
+                students[name].subjects[subject] = score
+            }
+        }
+        updateScore(name, subject, score){
+            if(students[name] && students[name].subjects[subject] !== undefined){
+                students[name].subjects[subject] = score
+            }
+        }
+        getStudentsDetails(name){
+            return students[name] || "Student not found"
+        }
+        getSortedStudents(sortBy){
+            return Object.entries(students).map()
+        }
+    }
+}
+
+
+// Task manager
+
+let taskManager = (function(){
+    let tasks = {}
+    let idCounter = 1;
+    return{
+        addTasks(title){
+            tasks.push({id: idCounter++, title, completed  : false})
+        }
+        getAllTasks(){
+            return JSON.stringify(tasks, null, 2)
+        }
+    }
+
+})
